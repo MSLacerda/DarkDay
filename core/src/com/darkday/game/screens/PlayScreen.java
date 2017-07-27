83ecaa268b94c85c1bf262da173e77cab6d92b29
Matheus Soares <the.matheus6@gmail.com>
2017-07-27 14:08:07 -0300

    Play screen done

---
+++ core/src/com/darkday/game/screens/PlayScreen.java
@@ -0,0 +1,50 @@
+package com.darkday.game.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.darkday.game.DarkDay;
+
+public class PlayScreen implements Screen {
+
+    Game game;
+
+    public PlayScreen(DarkDay game) {
+        this.game = game;
+    }
+ 
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
